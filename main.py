4175cedd72f48094da8154a3e03c8f42775e2180
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 20:53:09 +0200

    Final refactor

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import os
 __version__ = '1.0.5-beta'
 __author__ = 'Baziki#7133'
 
-
-# ascii characters used to build the output text
-ASCII_CHARS = "@#S%?*+;:,."
+ASCII_CHARS = "@#S%?*+;:,. "
+ASCII_WIDTH = 80  # terminal width
 
 TITLE = """
 .d8b.  .d8888.  .o88b. d888888b d888888b       .d8b.  d8888b. d888888b      d888888b  .d88b.   .d88b.  db      
@@ -20,40 +19,36 @@ YP   YP `8888Y'  `Y88P' Y888888P Y888888P      YP   YP 88   YD    YP            
 """
 
 
-# resize image according to a new width
-def resize_image(image, new_width=100):
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def resize_image(image, new_width=ASCII_WIDTH):
+    """Resize image according to a new width."""
     width, height = image.size
-    ratio = height / width
-    new_height = int(ratio * new_width * 0.55)
+    new_height = int((height / width) * new_width * 0.55)
     return image.resize((new_width, new_height))
 
 
-# convert each pixel to grayscale
-def gray_ify(image):
-    return image.convert("L")
-
-
-# convert pixels to a string of ascii characters
-def pta(image):
-    return ''.join(ASCII_CHARS[pixel // 25] for pixel in image.getdata())
+def pixel_to_ascii(image):
+    """Convert pixels to a string of ascii characters"""
+    return ''.join(ASCII_CHARS[int((pixel / 256) * len(ASCII_CHARS))] for pixel in image.getdata())
 
 
 def main():
     set_title(f'ASCII Art Convert - v{__version__} | by {__author__}')
     os.system('cls')
 
-    # attempt to open image from user-input
     print(TITLE)
     print(f"\nAuthor: {__author__}")
-    print(f"Session: {random.randint(0, 9999):04}\n")
-
     is_running = True
 
     while is_running:
         path = input("#if you want to exit type 'exit' or 'quit'\nImage Patch: ")
 
         if path in ['exit', 'quit', 'EXIT', 'QUIT']:
-            print("\nExiting....")
             is_running = False
 
         if not os.path.exists(path):
@@ -61,31 +56,21 @@ def main():
             continue
 
         image = Image.open(path)
-        convert_image(image)
+
+        print("Converting your image to ASCII....")
+        new_image_data = pixel_to_ascii(resize_image(image).convert("L"))
+
+        print("Your ASCII Image:")
+        print('\n'.join(chunks(new_image_data, ASCII_WIDTH)))
+
+        print("Your ASCII image has been saved to image.txt")
+        os.system("pause")
+
+        with open("image.txt", "w+") as f:
+            f.write(ascii_image)
+
+    print("Exiting....")
 
 
-def convert_image(image, new_width=100):
-    # convert image to ascii
-    print("\nConverting your image to ASCII....")
-    new_image_data = pta(gray_ify(resize_image(image)))
-
-    # format
-    print("\nFormat your ASCII image....")
-
-    pixel_count = len(new_image_data)
-    ascii_image = "\n".join(new_image_data[index: (index + new_width)] for index in range(0, pixel_count, new_width))
-
-    # print result
-    print("\nYour ASCII Image:")
-    print(ascii_image)
-    print("\nYour ASCII image has been saved to 'image.txt'")
-    os.system("pause")
-
-    # save result to "image.txt"
-    with open("image.txt", "w") as f:
-        f.write(ascii_image)
-
-
-# run program
 if __name__ == '__main__':
     main()
