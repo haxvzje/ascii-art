2529b6e3cb5f366bffbefca67aaba64ebd37c576
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 20:09:42 +0200

    Python simplifications

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import random
 import time
 from os import system
 
+__version__ = '1.0.5-beta'
+__author__ = 'Baziki#7133'
+
+
 # ascii characters used to build the output text
-ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+ASCII_CHARS = "@#S%?*+;:,."
 
 
 # resize image according to a new width
@@ -13,50 +17,52 @@ def resize_image(image, new_width=100):
     width, height = image.size
     ratio = height / width
     new_height = int(ratio * new_width * 0.55)
-    resized_image = image.resize((new_width, new_height))
-    return resized_image
+    return image.resize((new_width, new_height))
 
 
 # convert each pixel to grayscale
-def grayify(image):
-    grayscale_image = image.convert("L")
-    return grayscale_image
+def gray_ify(image):
+    return image.convert("L")
 
 
 # convert pixels to a string of ascii characters
 def pta(image):
     pixels = image.getdata()
-    characters = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
-    return characters
+    return "".join(ASCII_CHARS[pixel // 25] for pixel in pixels)
 
 
 def main(new_width=100):
     # attempt to open image from user-input
     print(logo + f"\nAuthor: {author}")
     print(f"Session: {se1}{se2}{se3}{se4}\n")
+
     while True:
         path = input("#if you want to exit type 'exit' or 'quit'\nImage Patch: ")
-        if path == 'exit' or path == 'quit' or path == 'EXIT' or path == 'QUIT':
+
+        if path in ['exit', 'quit', 'EXIT', 'QUIT']:
             print("\nExiting....")
             time.sleep(3)
             exit()
+
         try:
             image = Image.open(path)
             break
-        except:
-            print(path, "is not a valid path.")
-            pass
 
+        except Exception:
+            print(path, "is not a valid path.")
     # convert image to ascii
     print("\nConverting your image to ASCII....")
     time.sleep(3)
-    new_image_data = pta(grayify(resize_image(image)))
+    new_image_data = pta(gray_ify(resize_image(image)))
 
     # format
     print("\nFormat your ASCII image....")
     time.sleep(2)
     pixel_count = len(new_image_data)
-    ascii_image = "\n".join([new_image_data[index:(index + new_width)] for index in range(0, pixel_count, new_width)])
+    ascii_image = "\n".join(
+        new_image_data[index: (index + new_width)]
+        for index in range(0, pixel_count, new_width)
+    )
 
     # print result
     print("\nYour ASCII Image:")
@@ -71,19 +77,16 @@ def main(new_width=100):
 
 # run program
 if __name__ == '__main__':
-    logo = ''' .d8b.  .d8888.  .o88b. d888888b d888888b       .d8b.  d8888b. d888888b      d888888b  .d88b.   .d88b.  db      
-d8' `8b 88'  YP d8P  Y8   `88'     `88'        d8' `8b 88  `8D `~~88~~'      `~~88~~' .8P  Y8. .8P  Y8. 88      
-88ooo88 `8bo.   8P         88       88         88ooo88 88oobY'    88            88    88    88 88    88 88      
-88~~~88   `Y8b. 8b         88       88         88~~~88 88`8b      88            88    88    88 88    88 88      
-88   88 db   8D Y8b  d8   .88.     .88.        88   88 88 `88.    88            88    `8b  d8' `8b  d8' 88booo. 
-YP   YP `8888Y'  `Y88P' Y888888P Y888888P      YP   YP 88   YD    YP            YP     `Y88P'   `Y88P'  Y88888P '''
-    version = '1.0.5-beta'
-    author = 'Baziki#7133'
-    se1 = random.randint(0, 9)
-    se2 = random.randint(0, 9)
-    se3 = random.randint(0, 9)
-    se4 = random.randint(0, 9)
+    print("""
+        .d8b.  .d8888.  .o88b. d888888b d888888b       .d8b.  d8888b. d888888b      d888888b  .d88b.   .d88b.  db      
+        d8' `8b 88'  YP d8P  Y8   `88'     `88'        d8' `8b 88  `8D `~~88~~'      `~~88~~' .8P  Y8. .8P  Y8. 88      
+        88ooo88 `8bo.   8P         88       88         88ooo88 88oobY'    88            88    88    88 88    88 88      
+        88~~~88   `Y8b. 8b         88       88         88~~~88 88`8b      88            88    88    88 88    88 88      
+        88   88 db   8D Y8b  d8   .88.     .88.        88   88 88 `88.    88            88    `8b  d8' `8b  d8' 88booo. 
+        YP   YP `8888Y'  `Y88P' Y888888P Y888888P      YP   YP 88   YD    YP            YP     `Y88P'   `Y88P'  Y88888P
+    """)
 
-    set_title(f'ASCII Art Convert - v{version} | by {author}')
+    se1, se2, se3, se4 = (random.randint(0, 9) for _ in range(4))
+    set_title(f'ASCII Art Convert - v{__version__} | by {__author__}')
     system('cls')
     main()
