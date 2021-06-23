73645df0e75ac65324726d85f19dc54e7631f695
Yohann Boniface <edhyjox@gmail.com>
2021-06-23 20:56:35 +0200

    Typing

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from console.utils import set_title
 from PIL import Image
-import random
 import os
 
-__version__ = '1.0.5-beta'
-__author__ = 'Baziki#7133'
+__version__: str = '1.0.5-beta'
+__author__: str = 'Baziki#7133'
 
-ASCII_CHARS = "@#S%?*+;:,. "
-ASCII_WIDTH = 80  # terminal width
+ASCII_CHARS: str = "@#S%?*+;:,. "
+ASCII_WIDTH: int = 80  # terminal width
 
-TITLE = """
+TITLE: str = """
 .d8b.  .d8888.  .o88b. d888888b d888888b       .d8b.  d8888b. d888888b      d888888b  .d88b.   .d88b.  db      
 d8' `8b 88'  YP d8P  Y8   `88'     `88'        d8' `8b 88  `8D `~~88~~'      `~~88~~' .8P  Y8. .8P  Y8. 88      
 88ooo88 `8bo.   8P         88       88         88ooo88 88oobY'    88            88    88    88 88    88 88      
@@ -19,46 +18,46 @@ YP   YP `8888Y'  `Y88P' Y888888P Y888888P      YP   YP 88   YD    YP            
 """
 
 
-def chunks(lst, n):
+def chunks(lst: List[Any], n: int) -> List[Any]:
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
 
-def resize_image(image, new_width=ASCII_WIDTH):
+def resize_image(image: PIL.Image, new_width: int = ASCII_WIDTH) -> PIL.Image:
     """Resize image according to a new width."""
     width, height = image.size
     new_height = int((height / width) * new_width * 0.55)
     return image.resize((new_width, new_height))
 
 
-def pixel_to_ascii(image):
+def pixel_to_ascii(image: PIL.Image) -> str:
     """Convert pixels to a string of ascii characters"""
     return ''.join(ASCII_CHARS[int((pixel / 256) * len(ASCII_CHARS))] for pixel in image.getdata())
 
 
-def main():
+def main() -> NoReturn:
     set_title(f'ASCII Art Convert - v{__version__} | by {__author__}')
     os.system('cls')
 
     print(TITLE)
     print(f"\nAuthor: {__author__}")
-    is_running = True
+    is_running: bool = True
 
     while is_running:
-        path = input("#if you want to exit type 'exit' or 'quit'\nImage Patch: ")
+        path: str = input("#if you want to exit type 'exit' or 'quit'\nImage Patch: ")
 
-        if path in ['exit', 'quit', 'EXIT', 'QUIT']:
+        if path in {'exit', 'quit', 'EXIT', 'QUIT'}:
             is_running = False
 
         if not os.path.exists(path):
             print(path, "is not a valid path.")
             continue
 
-        image = Image.open(path)
+        image: PIL.Image = Image.open(path)
 
         print("Converting your image to ASCII....")
-        new_image_data = pixel_to_ascii(resize_image(image).convert("L"))
+        new_image_data: str = pixel_to_ascii(resize_image(image).convert("L"))
 
         print("Your ASCII Image:")
         print('\n'.join(chunks(new_image_data, ASCII_WIDTH)))
